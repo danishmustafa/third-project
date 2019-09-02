b2a23eb2573e43aeb3c91d7c667c9bbbe7fbb6e6
asp <asp@Aptechnk.edu>
2019-09-02 10:24:07 +0500

    commited by faizan qureshi

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("danish");
             Console.WriteLine("faizan");
+            Console.WriteLine("yousuf");
 
         }
     }
